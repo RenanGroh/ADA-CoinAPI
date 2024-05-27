7668088d76e6416a27beda4f5dc91a8c35e538b6
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-05-26 23:44:42 -0300

    Update QuoteControllerTest.java

--- src/test/java/com/dev/api/adacoinapi/controller/QuoteControllerTest.java
+++ src/test/java/com/dev/api/adacoinapi/controller/QuoteControllerTest.java
@@ -67,3 +67,35 @@ public class QuoteControllerTest {
         assertEquals(mockConversion, response);
     }
 }
+
+@WebMvcTest(QuoteController.class)
+@WithMockUser(roles = "USER")
+class QuoteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private QuoteService quoteService;
+
+    @Test
+    void testGetRealTimeQuotes() throws Exception {
+        Map<String, CurrencyQuote> quotes = new HashMap<>();
+        quotes.put("USD", new CurrencyQuote());
+        when(quoteService.getRealTimeQuotes(anyList())).thenReturn(quotes);
+
+        mockMvc.perform(get("/api/quotes")
+                .param("currencies", "USD,EUR"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testConvertCurrency() throws Exception {
+        CurrencyConversionDTO dto = new CurrencyConversionDTO("USD", "EUR", BigDecimal.valueOf(100), BigDecimal.valueOf(85));
+        when(quoteService.convertCurrency("USD", "EUR", BigDecimal.valueOf(100))).thenReturn(dto);
+
+        mockMvc.perform(get("/api/quotes/convert/{fromCurrency}/{toCurrency}/{amount}", "USD", "EUR", 100))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.convertedAmount").value("85"));
+    }
+}
